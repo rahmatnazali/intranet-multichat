e76d5f3ccd5e254bc82f6f2ca61db2a92e107ad1
rahmat nazali s <rahmatnazali95@gmail.com>
2017-04-09 20:02:57 +0700

    Manage directory

---
+++ source code/multichatserver/MultiChatGUIServer.java
@@ -0,0 +1,12 @@
+/**
+ * Created by rahmat
+ */
+
+package multichatguiserver;
+
+public class MultiChatGUIServer{    
+    public static void main(String args[]) {
+        ServerFrame serverframe = new ServerFrame();                            // buat objek ServerFrame, jalankan, dan buat agar terlihat
+        serverframe.setVisible(true);
+  }
+}
